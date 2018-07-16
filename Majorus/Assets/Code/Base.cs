f59ca531d0a7285e8cbb49574db578a2bf1a10b4
AJKaufman <ak3012@rit.edu>
2018-07-15 22:04:08 -0400

    Starded a random file to warm up

---
+++ Majorus/Assets/Code/Base.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base : MonoBehaviour {
+
+  public int money;
+  public float mana;
+  public String[] Swords = ["Maramusa", "Scarred Cleaver", "Mutton Chop", "Massive Lance", "Agile Corsair", "Equalizer"];
+
+  // Use this for initialization
+  void Start () {
+
+  }
+
+  // Update is called once per frame
+  void Update () {
+
+  }
+
+  // Get a sword
+  public String GetRandomSword() {
+    
+  }
+    
+}
